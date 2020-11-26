0d677909118c5c4ad5d142f1c9c5a7ae5a47a523
Francisco <javiermartincuenca@gmail.com>
2020-11-26 18:14:30 +0100

    Optimize for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+      content: ['./src/**/*.vue', './public/index.html'],
+      defaultExtractor: content => content.match(/[A-Zaz0-9-_:/]+/g) || []
+    }),
+  ],
 };
